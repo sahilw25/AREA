d2113b1d425ffb58e9cd7b9ef69ab129c749c00c
sahilw25 <walawalkarsahil25@gmail.com>
2024-07-16 19:21:34 +0530

    Area v7

--- public/script/variant.js
+++ public/script/variant.js
@@ -1,22 +1,42 @@
 
-const modelViewerVariants = document.querySelector("model-viewer#shoe");
-const select = document.querySelector('#variant');
+// const modelViewerVariants = document.querySelector("model-viewer#shoe");
+// const select = document.querySelector('#variant');
 
-modelViewerVariants.addEventListener('load', () => {
-  const names = modelViewerVariants.availableVariants;
-  for (const name of names) {
-    const option = document.createElement('option');
-    option.value = name;
-    option.textContent = name;
-    select.appendChild(option);
-  }
-  // Adds a default option.
-  const option = document.createElement('option');
-    option.value = 'default';
-    option.textContent = 'Default';
-    select.appendChild(option);
-});
+// modelViewerVariants.addEventListener('load', () => {
+//   const names = modelViewerVariants.availableVariants;
+//   for (const name of names) {
+//     const option = document.createElement('option');
+//     option.value = name;
+//     option.textContent = name;
+//     select.appendChild(option);
+//   }
+//   // Adds a default option.
+//   const option = document.createElement('option');
+//     option.value = 'default';
+//     option.textContent = 'Default';
+//     select.appendChild(option);
+// });
 
-select.addEventListener('input', (event) => {
-  modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
-});
+// select.addEventListener('input', (event) => {
+//   modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
+// });
+
+
+
+
+
+  const modelViewerVariants = document.querySelector("model-viewer"); 
+  const select = document.querySelector('#variant'); 
+  modelViewerVariants.addEventListener('load', () => { 
+    const names = modelViewerVariants.availableVariants; 
+    for (const name of names) { 
+      const color = document.createElement('button'); 
+      color.value = name; 
+      color.textContent = name; 
+      color.classList.add('button-35');
+      select.appendChild(color); 
+    } 
+  }); 
+    document.getElementById('variant').addEventListener('click', (event) => { modelViewerVariants.variantName = event.target.value;
+  });
+
